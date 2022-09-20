aad076f43f98753843bbb272167539c3a38c5b4d
rtexelm <texelist@gmail.com>
2022-09-20 14:00:09 -0400

    Added part 1 of hangman.rb

--- hangman_project/lib/hangman.rb
+++ hangman_project/lib/hangman.rb
@@ -1,4 +1,42 @@
 class Hangman
   DICTIONARY = ["cat", "dog", "bootcamp", "pizza"]
 
+  def self.random_word
+    DICTIONARY.sample
+  end
+  
+
+  def initialize
+    @secret_word = Hangman.random_word
+    @guess_word = Array.new(@secret_word.length, "_")
+    @attempted_chars = []
+    @remaining_incorrect_guesses = 5
+  end
+
+  def guess_word
+    @guess_word
+  end
+
+  def attempted_chars
+    @attempted_chars
+  end
+
+  def remaining_incorrect_guesses
+    @remaining_incorrect_guesses
+  end
+
+  def already_attempted?(char)
+    @attempted_chars.include?(char)
+  end
+
+  def get_matching_indices(char)
+    (0...@secret_word.length).select {|i| @secret_word[i].eql?(char)}
+  end
+
+  def fill_indices(char, inds)
+    inds.each{|i| @guess_word[i] = char}
+  end
+
+
+
 end
